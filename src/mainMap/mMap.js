3fa4770d2335304841304ff468992b7640a68ad1
Hafeefa <hafeefa40@gmail.com>
2023-12-05 23:24:07 -0500

    added note for users to 100% know how to reserve a spot

--- src/mainMap/mMap.js
+++ src/mainMap/mMap.js
@@ -25,6 +25,16 @@ function mMap({ spots }) {
   return (
     <div className="mapContainer">
       <div className="tableWrapper">
+      <p style={{
+          textAlign: "center",
+          backgroundColor: "aliceblue",
+          borderRadius: "28px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        }}>
+          Click on a parking spot to reserve it. If you find a spot you'd like,
+          click on the number, and you'll be prompted to confirm your reservation.
+        </p>
+
         <table className="mMap">
           <tbody>{rows}</tbody>
         </table>
